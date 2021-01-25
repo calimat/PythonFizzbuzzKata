53f1cabdd32aceacc4a4d584b3eeea7a5e4c132f
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-25 14:18:47 -0500

    changed the name of the third test

--- fizbuzzKataTests.py
+++ fizbuzzKataTests.py
@@ -1,7 +1,7 @@
 import unittest
 
 def transform(number):
-        return number
+    return number
 
 class TestSum(unittest.TestCase):
     def test_transform_returnsArrayValueWith1(self):
@@ -12,7 +12,7 @@ class TestSum(unittest.TestCase):
         array = [1,2]
         result = transform(array)
         self.assertEqual(result, [1,2])
-    def test_transform_returnsArrayValueWith1And2(self):
+    def test_transform_returnsArrayValueWith1And2AndFizz(self):
         array = [1,2, 3]
         result = transform(array)
         self.assertEqual(result, [1,2, "Fizz"])
