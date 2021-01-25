3e99a93de4479e71a80f1dc4203eaaa0dad286a7
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-25 14:06:49 -0500

    Added a test for the 1,2,Fizz tests

--- fizbuzzKataTests.py
+++ fizbuzzKataTests.py
@@ -12,6 +12,10 @@ class TestSum(unittest.TestCase):
         array = [1,2]
         result = transform(array)
         self.assertEqual(result, [1,2])
+    def test_transform_returnsArrayValueWith1And2(self):
+        array = [1,2, 3]
+        result = transform(array)
+        self.assertEqual(result, [1,2, "Fizz"])
 
 if __name__ == '__main__':
     unittest.main()
